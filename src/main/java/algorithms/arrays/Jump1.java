937fc2ac1d1f80c91ee2a233e100db582aa86db9
vakeel <vakeel.ai.2006@gmail.com>
2026-06-19 10:57:18 +0530

    jump

---
+++ src/main/java/algorithms/arrays/Jump1.java
@@ -0,0 +1,27 @@
+package algorithms.arrays;
+
+public class Jump1 {
+
+    public boolean canJump(int[] nums) {
+
+        int farthest = 0;
+
+        for(int i=0;i<nums.length;i++){
+
+
+            if(i>farthest) {
+                return false;
+            }
+
+            farthest = Math.max(farthest,i+nums[i]);
+
+            if(farthest >= nums.length-1){
+                return true;
+            }
+
+        }
+        return false;
+
+    }
+
+}
